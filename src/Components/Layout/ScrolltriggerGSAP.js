0e14514b0edeb2b12967400a17428113534bf456
Veena Krishna Sandana <veenakrishna.s@gmail.com>
2021-05-03 16:33:19 -0700

    animation of nodes without scroll done

--- src/Components/Layout/ScrolltriggerGSAP.js
+++ src/Components/Layout/ScrolltriggerGSAP.js
@@ -10,6 +10,7 @@ import img3 from "../Scrollytelling/assets/Avatars/3.png";
 import img4 from "../Scrollytelling/assets/Avatars/4.png";
 import img5 from "../Scrollytelling/assets/Avatars/5.png";
 import img6 from "../Scrollytelling/assets/Avatars/6.png";
+import img7 from "../Scrollytelling/assets/Avatars/7.png";
 
 export default function ScrolltriggerGSAP() {
 
@@ -25,7 +26,7 @@ export default function ScrolltriggerGSAP() {
       .to("#img1", {
         x: 300,
         duration: 2,
-        scale: 0.5,
+        scale: 0.6,
         delay: 2,
         ScrollTrigger: {
           trigger: "img1",
@@ -35,15 +36,41 @@ export default function ScrolltriggerGSAP() {
       .fromTo(
         "#img2",
         { x: 0, y: 0, duration: 3, opacity: 0 },
-        { x: 500, y: -350, duration: 3, opacity: 1 }
+        { x: 130, y: -350, duration: 3, opacity: 1 , ScrollTrigger: {
+          trigger: "img1",
+          marker: true,
+        } }
       )
-      .to("#img2", { scale: 0.5, duration: 1 })
+      .to("#img2", { scale: 0.6, duration: 1 })
       .fromTo(
         "#img3",
         { x: 0, y: 0, duration: 1, opacity: 0 },
-        { x: 250, y: -700, duration: 3, opacity: 1 }
+        { x: 450, y: -860, duration: 3, opacity: 1 }
       )
-      .to("#img3", { scale: 0.5, duration: 1 });
+      .to("#img3", { scale: 0.6, duration: 1 })
+      .fromTo(
+        "#img4",
+        { x: 0, y: 0, duration: 3, opacity: 0 },
+        { x: 700, y: -1365, duration: 3, opacity: 1 }
+      )
+      .to("#img4", { scale: 0.6, duration: 1 })
+      .fromTo(
+        "#img5",
+        { x: 0, y: 0, duration: 3, opacity: 0 },
+        { x: 250, y: -1700, duration: 3, opacity: 1 }
+      )
+      .to("#img5", { scale: 0.6, duration: 1 })
+      .fromTo(
+        "#img6",
+        { scale: 0.6, x: 0, y: 0, duration: 3, opacity: 0 },
+        { x: 450, y: -2200, duration: 3, opacity: 1 }
+      )
+      .fromTo(
+        "#img7",
+        { scale: 0.6, x: 0, y: 0, duration: 3, opacity: 0 },
+        { x: 600, y: -2700, duration: 3, opacity: 1 }
+      );
+      
     
 
     
@@ -58,9 +85,11 @@ export default function ScrolltriggerGSAP() {
         <CardImg ref={ref} id="img1" src={img1} />
         <CardImg ref={ref} id="img2" src={img2} />
         <CardImg ref={ref} id="img3" src={img3} />
+        <CardImg ref={ref} id="img4" src={img4} />
+        <CardImg ref={ref} id="img5" src={img5} />
+        <CardImg ref={ref} id="img6" src={img6} />
+        <CardImg ref={ref} id="img7" src={img7} />
       </Card>
-     
-     
     </div>
   );
 }
